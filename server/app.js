047fe286a4da51ed9ef7914811618cd3d8da184a
saud <gomail2017@gmail.com>
2022-01-17 23:04:56 +0500

    setting out folder route

--- server/app.js
+++ server/app.js
@@ -10,10 +10,20 @@ const PORT = process.env.PORT || 4000
 app.use(express.json())
 app.use(require('./router/route'))
 
+// // Accessing the path module
+// const path = require("path");
+
+// // Step 1:
+// app.use(express.static(path.resolve(__dirname, "./mernfront/out")));
+// // Step 2:
+// app.get("*", function (request, response) {
+//   response.sendFile(path.resolve(__dirname, "./mernfront/out", "index.html"));
+// });
+
 app.listen(PORT, ()=>{
     console.log(`server is running at ${PORT}`)
 })
 
 if(process.env.NODE_ENV == "production" ){
-    app.use(express.static("out"));
+    app.use(express.static("./mernfront/out"));
 }
